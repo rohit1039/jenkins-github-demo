4a0ec859cb723639e3dc0a62824a553e8e95e4bb
rohit0599 <rohit.parida@wissen.com>
2024-07-05 12:20:20 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("******** Hello from Java *********");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello: " + i);
+        }
+    }
+}
